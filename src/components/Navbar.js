e5144d901086933807b312353dd569355cfdbf59
Nitesh Chowdhary <thenitesh0112@gmail.com>
2023-08-09 00:54:09 +0530

    redirects

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,5 +1,4 @@
-import { Fragment } from "react";
-import { Disclosure, Menu, Transition } from "@headlessui/react";
+import { Disclosure, Menu } from "@headlessui/react";
 import { Bars3Icon, XMarkIcon } from "@heroicons/react/24/outline";
 import { Link } from "react-router-dom";
 
